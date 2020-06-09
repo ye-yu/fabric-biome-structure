507c4b43d36c4cbf41da2f957f9cfb852c8f0f84
ye-yu <rafolwen98@gmail.com>
2020-06-09 11:23:06 +0800

    Init

---
+++ src/main/java/fp/yeyu/mcstructures/biomestructures/BiomeStructures.java
@@ -0,0 +1,12 @@
+package fp.yeyu.mcstructures.biomestructures;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.gen.feature.ShipwreckFeature;
+
+public class BiomeStructures implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ShipwreckFeature
+
+    }
+}
