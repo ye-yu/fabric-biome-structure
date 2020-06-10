f4a5f3510f726ce02be4e7f77b653d4526c677f8
ye-yu <rafolwen98@gmail.com>
2020-06-10 14:33:10 +0800

    Refactor codes

--- src/main/java/fp/yeyu/mcstructures/biomestructures/BiomeStructures.java
+++ src/main/java/fp/yeyu/mcstructures/biomestructures/BiomeStructures.java
@@ -7,7 +7,6 @@ import net.minecraft.structure.StructurePieceType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.ChanceDecoratorConfig;
 import net.minecraft.world.gen.decorator.Decorator;
@@ -19,8 +18,27 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BiomeStructures implements ModInitializer {
-    private static final Logger LOGGER = LogManager.getLogger();
     public static final String NAMESPACE = "biomestructures";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    static {
+        for (Biome biome : Registry.BIOME) {
+            if (biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.RIVER) {
+                biome.addStructureFeature((Features.CEMETERY.getStructure()).configure(new DefaultFeatureConfig()));
+                biome.addFeature(GenerationStep.Feature.SURFACE_STRUCTURES, Features.CEMETERY.getFeature().configure(FeatureConfig.DEFAULT).createDecoratedFeature(Decorator.CHANCE_PASSTHROUGH.configure(new ChanceDecoratorConfig(0))));
+            }
+        }
+    }
+
+    public static Identifier constructIdentifier(String nameItem) {
+        return new Identifier(NAMESPACE, nameItem);
+    }
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Mod is loaded.");
+    }
+
 
     public enum Generators {
         CEMETERY(constructIdentifier("cemetery_generator"), CemeteryGenerator.Piece::new);
@@ -56,24 +74,5 @@ public class BiomeStructures implements ModInitializer {
         public StructureFeature<DefaultFeatureConfig> getStructure() {
             return structure;
         }
-    }
-
-    static {
-        for(Biome biome : Registry.BIOME) {
-            if(biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.RIVER) {
-                biome.addStructureFeature((Features.CEMETERY.getStructure()).configure(new DefaultFeatureConfig()));
-                biome.addFeature(GenerationStep.Feature.SURFACE_STRUCTURES, Features.CEMETERY.getFeature().configure(FeatureConfig.DEFAULT).createDecoratedFeature(Decorator.CHANCE_PASSTHROUGH.configure(new ChanceDecoratorConfig(0))));
-            }
-        }
-    }
-
-
-    public static Identifier constructIdentifier(String nameItem) {
-        return new Identifier(NAMESPACE, nameItem);
-    }
-
-    @Override
-    public void onInitialize() {
-        LOGGER.info("Mod is loaded.");
     }
 }
--- src/main/java/fp/yeyu/mcstructures/biomestructures/feature/CemeteryFeature.java
+++ src/main/java/fp/yeyu/mcstructures/biomestructures/feature/CemeteryFeature.java
@@ -3,23 +3,15 @@ package fp.yeyu.mcstructures.biomestructures.feature;
 import com.mojang.datafixers.Dynamic;
 import fp.yeyu.mcstructures.biomestructures.BiomeStructures;
 import fp.yeyu.mcstructures.biomestructures.generator.CemeteryGenerator;
-import net.minecraft.structure.IglooGenerator;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.structure.StructureStart;
-import net.minecraft.structure.VillageStructureStart;
-import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockBox;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.source.BiomeAccess;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.AbstractTempleFeature;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.IglooFeature;
 import net.minecraft.world.gen.feature.StructureFeature;
 
-import java.util.Random;
 import java.util.function.Function;
 
 public class CemeteryFeature extends AbstractTempleFeature<DefaultFeatureConfig> {
